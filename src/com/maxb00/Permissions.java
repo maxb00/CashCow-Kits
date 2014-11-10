4747676686cf48631bfbe6c7a241c81528e595ed
maxb00 <maxbar00@yahoo.com>
2014-11-09 19:40:22 -0500

    Initial Upload
    Local version 3, git version 1

---
+++ src/com/maxb00/Permissions.java
@@ -0,0 +1,14 @@
+package com.maxb00;
+
+import org.bukkit.permissions.Permission;
+
+public class Permissions {
+
+	public Permission kitPvp = new Permission("maxb00.kit.pvp");
+	public Permission kitMage = new Permission("maxb00.kit.mage");
+	public Permission kitArcher = new Permission("maxb00.kit.archer");
+	public Permission kitUndead = new Permission("maxb00.kit.undead");
+	public Permission kitJoker = new Permission("maxb00.kit.joker");
+	public Permission rainbow = new Permission("maxb00.lobby.rainbow");
+
+}
